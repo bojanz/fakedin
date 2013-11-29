c9452f123e304649517a49cbf8ea87427e9c3119
Bojan Zivanovic <bojanz@gmail.com>
2013-11-30 00:36:59 +0100

    Add the fakedin license type which extends the role license type with usage tracking and a custom checkout completion message.

---
+++ modules/features/fakedin_core/plugins/type/CommerceLicenseFakedIn.class.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * FakedIn license type.
+ * Extends the role license type with usage tracking and a custom
+ * checkout completion message.
+ */
+class CommerceLicenseFakedIn extends CommerceLicenseRole implements CommerceLicenseBillingUsageInterface  {
+
+  /**
+   * Implements CommerceLicenseBillingUsageInterface::usageGroups().
+   */
+  public function usageGroups() {
+    return array(
+      'posts' => array(
+        'title' => t('Posts'),
+        'type' => 'counter',
+        'product' => $this->wrapper->product->field_usage_product->sku->value(),
+        'free_quantity' => $this->wrapper->product->field_free_posts_quota->value(),
+      ),
+    );
+  }
+
+  /**
+   * Implements CommerceLicenseBillingUsageInterface::usageDetails().
+   */
+  public function usageDetails() {
+    $usage = commerce_license_billing_current_usage($this, 'posts');
+    return t('Posts: @posts', array('@posts' => $usage));
+  }
+
+  /**
+   * Implements CommerceLicenseInterface::checkoutCompletionMessage().
+   */
+  public function checkoutCompletionMessage() {
+    $message = 'Thank you for becoming a FakedIn member! <br />';
+    $message = 'You can now ' . l('edit your profile', 'profile/' . $this->uid . '/edit') . '.';
+    return $message;
+  }
+}
